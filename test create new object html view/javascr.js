ef33f2bf595d4bb8c934b2a13d81aab593f413b5
dent1p <bogaartmark@gmail.com>
2019-07-14 13:51:11 +0200

    Preliminary test array of objects html view

--- test create new object html view/javascr.js
+++ test create new object html view/javascr.js
@@ -2,11 +2,30 @@
 "use strict"
 
 
-const x = [];
+let x = [];
 document.querySelector('#todo-form').addEventListener('submit', function (e) {
   e.preventDefault();
   
   x.push({ FirstName: e.target.elements.firstname.value, 
   LastName: e.target.elements.lastname.value });
+  e.target.elements.firstname.value = '';
+  e.target.elements.lastname.value = '';
 })
 console.log(x)
+
+
+document.querySelector('#todo-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  let x = [];
+  x.push({
+    FirstName: e.target.elements.firstname.value,
+    LastName: e.target.elements.lastname.value
+  });
+
+  for (let o of x){
+    console.log(o)
+  }
+  e.target.elements.firstname.value = '';
+  e.target.elements.lastname.value = '';
+})
+
